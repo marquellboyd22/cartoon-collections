cab83c0f65003f61fcad5cba1e54ea6820bf3f2c
Marquell boyd <marquellboyd@icloud.com>
2020-10-09 07:05:28 -0400

    Update cartoon_collections.rb

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  roll_call = []
+  dwarves.each do |dwarf|
+    roll_call << "#{(dwarves.index(dwarf) + 1)}. #{dwarf}"
+  end
+  puts "#{roll_call.join(" ")}"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array) 
+  array.map! {|planet| planet.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any?{|i| i.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese| 
+    cheese_types.include?(cheese)
+  end
 end
